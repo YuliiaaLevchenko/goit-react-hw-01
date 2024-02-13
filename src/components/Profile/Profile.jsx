f9195b999dde30e3a09af15541bd45e8880f0652
YuliiaaLevchenko <141173167+YuliiaaLevchenko@users.noreply.github.com>
2024-02-13 15:54:41 +0200

    profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,30 +1,33 @@
-export const Profile = () => {
+
+export const Profile = ({ 
+  name, tag, location, image, stats }) => {
     return (
       <div>
-      <div>
-      <img
-      src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-      alt="User avatar"
-      />
-      <p>Petra Marica</p>
-      <p>@pmarica</p>
-      <p>Salvador, Brasil</p>
-      </div>
-     
-      <ul>
-      <li>
-      <span>Followers</span>
-      <span>1000</span>
-      </li>
-      <li>
-      <span>Views</span>
-      <span>2000</span>
-      </li>
-      <li>
-      <span>Likes</span>
-      <span>3000</span>
-      </li>
-      </ul>
-     </div>
+ <div>
+ <img
+ src={ image } 
+ alt="User avatar"
+ />
+ <p>{ name }</p>
+ <p>{ tag }</p>
+ <p>{ location }</p>
+ </div>
+
+ <ul>
+ <li>
+ <span>Followers</span>
+ <span>{ stats.follwers }</span>
+ </li>
+ <li>
+ <span>Views</span>
+ <span>{ stats.views }</span>
+ </li>
+ <li>
+ <span>Likes</span>
+ <span>{ stats.likes }</span>
+ </li>
+ </ul>
+</div>
+
     );
   };
